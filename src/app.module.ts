7a7ae2fbd7f0c498fcd16defa489504b2eeb7700
hejjzaen <vinnikov.p95@mail.ru>
2021-12-15 20:28:37 +0300

    Sequelize & env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-    imports: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            models: [],
+        })
+    ],
     controllers: [],
     providers: [],
 })
